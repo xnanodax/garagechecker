dac0e16b36a26fbd51b06b3b432cd562ed2bf89a
Cynthia Ma <cynthia.ma.emily@gmail.com>
2018-06-02 16:34:04 -0700

    Script can detect when the circuit's status has changed

--- blink.py
+++ blink.py
@@ -2,14 +2,32 @@ import time
 import RPi.GPIO as GPIO
 
 red=11
+status=13
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(red, GPIO.OUT)
+GPIO.setup(status, GPIO.IN)
 
 print(">> starting file")
 
+old_status = GPIO.input(status)
+
+GPIO.output(red, True)
+curr_status = GPIO.input(status)
+time.sleep(2)
+
+print(">> starting script")
+
 try:
 	while True:
-		GPIO.output(red, True)
+		if old_status != curr_status:
+			if curr_status > 0:
+				print("garage door closed")
+			else:
+				print("garage door open")			
+			old_status = curr_status
+		curr_status = GPIO.input(status)
+		time.sleep(0.05)
 except (KeyboardInterrupt, SystemExit):
+	GPIO.output(red, False)
 	GPIO.cleanup()
 	print("\n>> ending file")
