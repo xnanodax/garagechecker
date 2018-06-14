dcd30a28bbfc77fb0ed300e15f340516f1995baa
Cynthia Ma <cynthia.ma.emily@gmail.com>
2018-06-13 22:57:41 -0700

    Attempt to remove event listeners 3

--- test/onoff.js
+++ test/onoff.js
@@ -4,6 +4,7 @@ var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
 
 
 LED.watch(function(err, value) {
+  console.log("status", LED.readSync());
   if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
     LED.writeSync(1); //set pin state to 1 (turn LED on)
   } else {
@@ -12,6 +13,7 @@ LED.watch(function(err, value) {
 })
 
 process.on('SIGINT', function () {
+  console.log('exporting');
   LED.unexport();
 });
 
