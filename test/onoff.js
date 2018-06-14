75e6a9a6fa29a26bc5fce90dad2779047c0ff9a1
Cynthia Ma <cynthia.ma.emily@gmail.com>
2018-06-13 23:11:53 -0700

    Attempt to remove event listeners 4

--- test/onoff.js
+++ test/onoff.js
@@ -1,21 +1,27 @@
-var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
-// var blinkInterval = setInterval(blinkLED, 1000); //run the blinkLED function every 250ms
+var onoff = require('onoff'); //#A
 
+var Gpio = onoff.Gpio,
+  led = new Gpio(4, 'out'), //#B
+  interval;
 
-LED.watch(function(err, value) {
-  console.log("status", LED.readSync());
-  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
-    LED.writeSync(1); //set pin state to 1 (turn LED on)
-  } else {
-    LED.writeSync(0); //set pin state to 0 (turn LED off)
-  }  
-})
+interval = setInterval(function () { //#C
+  var value = (led.readSync() + 1) % 2; //#D
+  led.write(value, function() { //#E
+    console.log("Changed LED state to: " + value);
+  });
+}, 2000);
 
-process.on('SIGINT', function () {
-  console.log('exporting');
-  LED.unexport();
+process.on('SIGINT', function () { //#F
+  clearInterval(interval);
+  led.writeSync(0); //#G
+  led.unexport();
+  console.log('Bye, bye!');
+  process.exit();
 });
+
+// var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+// var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+// var blinkInterval = setInterval(blinkLED, 1000); //run the blinkLED function every 250ms
 
 // function blinkLED() { //function to start blinking
 //   if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
